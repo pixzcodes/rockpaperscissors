a9b2e583caf94bd499260d467e664da211b70e25
Pixz <tinycactusking@gmail.com>
2022-07-21 00:18:04 -0400

    Add score keeper and win conditions

--- script.js
+++ script.js
@@ -13,54 +13,57 @@ function getComputerChoice(){ // gets a random number between 0 and 2 and define
         return 'scissors';
     }
 }
-
-function playRound(playerSelection, computerSelection){
+var computerScore = 0; // Score Initializers 
+var playerScore = 0;
+function playRound(playerSelection, computerSelection){ //function to determine who wins the round
+    
     if (playerSelection == 'rock' && computerSelection == 'paper'){
-        var computerWins = true;
-        return 'Paper covers rock! Computer wins!';
+        computerScore += 1;
+        return'Paper covers rock! Computer wins!';
     }
     if (playerSelection == 'paper' && computerSelection == 'rock'){
-        var playerWins = true;
+        playerScore += 1;
         return 'Paper covers rock! Player wins!';
     }
     if (playerSelection == 'rock' && computerSelection == 'scissors'){
-        var playerWins = true;
+        playerScore += 1;
         return 'Rock smashes scissors! Player wins!';
     }
     if (playerSelection == 'scissors' && computerSelection == 'rock'){
-        var computerWins = true;
+        computerScore += 1;
         return 'Rock smashes scissors! Computer wins!';
     }
     if (playerSelection == 'paper' && computerSelection == 'scissors'){
-        var computerWins = true;
+        computerScore += 1;
         return 'Scissors cuts paper! Computer wins!';
     }
     if (playerSelection == 'scissors' && computerSelection == 'paper'){
-        var playerWins = true;
+        playerScore += 1;
         return 'Scissors cuts paper! Player wins!';
     } else{
-        return 'Its a tie!'
+        return 'The round is a tie!'
     }
     
 }
 
-function game () {
-    var computerScore = 0;
-    var playerScore = 0;
-    for (let i = 0; i < 5; i++){
-        var playerSelection = prompt('Please choose Rock, Paper, or Scissors...').toLowerCase();
-        var computerSelection = getComputerChoice();
+function game () { // main game function
+    
+    for (let i = 0; i < 5; i++){ //main game loop
+        var playerSelection = prompt('Please choose Rock, Paper, or Scissors...').toLowerCase(); //prompt player for their choice
+        var computerSelection = getComputerChoice(); // get computer choice
         console.log(playRound(playerSelection, computerSelection));
         
-        if (computerWins = true){
-            computerScore = computerScore + 1;
-        }
-        if (playerWins = true){
-            playerScore = playerScore + 1;
-        }
-        console.log(`The score is Computer: ${computerScore}, Player: ${playerScore}!`)
+        console.log(`The score is Computer: ${computerScore}, Player: ${playerScore}!`) // keep score
+        
+    }
+    console.log('Game Over! The Winner is...') //declare winner
+    if (computerScore > playerScore){
+        console.log('The Computer!')
+    }
+    if (playerScore > computerScore){
+        console.log('The Player!')
     }
 }
 
 
-game();
+game(); // start game
