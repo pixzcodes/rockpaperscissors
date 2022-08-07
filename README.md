9fa57219b37698200e3959298208256b18c425c4
Pixz <tinycactusking@gmail.com>
2022-08-06 22:51:59 -0400

    Add UI to rps game

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 A simple game of Rock, Paper, Scissors
 I will be making a simple Rock, Paper, Scissors game in javascript.
 I plan to add a gui to it later on but for now it will just be played through the console.
+
+## UPDATE
+I have added a gui to the game including buttons and text to keep track of things like choices and score
--- script.js
+++ script.js
@@ -1,72 +1,98 @@
 
-
+const computerChoiceMessage = document.querySelector('#computerChoiceMessage');
 function getComputerChoice(){ // gets a random number between 0 and 2 and defines the choice of the computer based on the number
     var choice = Math.floor(Math.random() * 3);
     if (choice === 0){
-        console.log('The Computer has chosen Rock!')
+        computerChoiceMessage.textContent = 'The Computer has chosen Rock!';
         return 'rock';
     } else if (choice === 1){
-        console.log('The Computer has chosen Paper!')
+        computerChoiceMessage.textContent = 'The Computer has chosen Paper!';
         return 'paper';
     } else {
-        console.log('The Computer has chosen Scissors!')
+        computerChoiceMessage.textContent = 'The Computer has chosen Scissors!';
         return 'scissors';
     }
 }
+const roundMessage = document.querySelector('#roundMessage');
 var computerScore = 0; // Score Initializers 
 var playerScore = 0;
-function playRound(playerSelection, computerSelection){ //function to determine who wins the round
+function playRoundRock(){
+    let computerSelection = getComputerChoice();
+
+    if (computerSelection == 'paper'){
+        computerScore += 1;
+        getScore();
+        roundMessage.textContent = 'Paper covers rock! Computer wins!';
+    }
+    if (computerSelection == 'scissors'){
+        playerScore += 1;
+        getScore();
+        roundMessage.textContent = 'Rock smashes scissors! Player wins!';
+    }
+    if (computerSelection == 'rock'){
+        roundMessage.textContent = 'Its a tie!';
+    }
+}
+function playRoundPaper(){
+    let computerSelection = getComputerChoice();
+
+    if (computerSelection == 'rock'){
+        playerScore += 1;
+        getScore();
+        roundMessage.textContent = 'Paper covers rock! Player wins!';
+    }
+    if (computerSelection == 'scissors'){
+        computerScore += 1;
+        getScore();
+        roundMessage.textContent = 'Scissors cuts paper! Computer wins!';
+    }
+    if (computerSelection == 'paper'){
+        roundMessage.textContent = 'Its a tie!';
+    }
+}
+function playRoundScissors(){ 
+    let computerSelection = getComputerChoice();
     
-    if (playerSelection == 'rock' && computerSelection == 'paper'){
+    if (computerSelection == 'rock'){
         computerScore += 1;
-        return'Paper covers rock! Computer wins!';
+        getScore();
+        roundMessage.textContent = 'Rock smashes scissors! Computer wins!';
     }
-    if (playerSelection == 'paper' && computerSelection == 'rock'){
+    
+    if (computerSelection == 'paper'){
         playerScore += 1;
-        return 'Paper covers rock! Player wins!';
+        getScore();
+        roundMessage.textContent = 'Scissors cuts paper! Player wins!';
     }
-    if (playerSelection == 'rock' && computerSelection == 'scissors'){
-        playerScore += 1;
-        return 'Rock smashes scissors! Player wins!';
-    }
-    if (playerSelection == 'scissors' && computerSelection == 'rock'){
-        computerScore += 1;
-        return 'Rock smashes scissors! Computer wins!';
-    }
-    if (playerSelection == 'paper' && computerSelection == 'scissors'){
-        computerScore += 1;
-        return 'Scissors cuts paper! Computer wins!';
-    }
-    if (playerSelection == 'scissors' && computerSelection == 'paper'){
-        playerScore += 1;
-        return 'Scissors cuts paper! Player wins!';
-    } else{
-        return 'The round is a tie!'
+    if (computerSelection == 'scissors'){
+        roundMessage.textContent = 'Its a tie!';
     }
     
 }
 
-function game () { // main game function
-    
-    for (let i = 0; i < 5; i++){ //main game loop
-        var playerSelection = prompt('Please choose Rock, Paper, or Scissors...').toLowerCase(); //prompt player for their choice
-        var computerSelection = getComputerChoice(); // get computer choice
-        console.log(playRound(playerSelection, computerSelection));
-        
-        console.log(`The score is Computer: ${computerScore}, Player: ${playerScore}!`) // keep score
-        
+const scoreMessage = document.querySelector('#scoreMessage');
+function getScore(){
+    scoreMessage.textContent = `The Score is Computer: ${computerScore}, Player: ${playerScore}`;
+    if (playerScore === 5){
+        winnerText.textContent = 'Congrats Player you have Beat the Computer!';
+        container.removeChild(rockBtn);
+        container.removeChild(paperBtn);
+        container.removeChild(scissorsBtn);
     }
-    console.log('Game Over! The Winner is...') //declare winner
-    if (computerScore > playerScore){
-        console.log('The Computer!')
-    }
-    if (playerScore > computerScore){
-        console.log('The Player!')
-    }
-    if (playerScore == computerScore){
-        console.log('No one! Its a tie!')
+    if (computerScore === 5){
+        winnerText.textContent = 'Darn! Looks like the Computer has gotten the best of you Player!';
+        container.removeChild(rockBtn);
+        container.removeChild(paperBtn);
+        container.removeChild(scissorsBtn);
     }
 }
 
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorsBtn = document.querySelector('#scissors');
 
-game(); // start game
+const winnerText = document.querySelector('#winnerText');
+const container = document.querySelector('#container');
+rockBtn.addEventListener('click', playRoundRock);
+paperBtn.addEventListener('click', playRoundPaper);
+scissorsBtn.addEventListener('click', playRoundScissors);
